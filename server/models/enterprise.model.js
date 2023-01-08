a4244544fc5f7a332d86872f56ba391cc49fa36f
Eriuden <antoinecarrette@hotmail.fr>
2023-01-08 11:00:02 +0100

    nowWithModels

---
+++ server/models/enterprise.model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose")
+
+const enterpriseSchema = new mongoose.Schema(
+    {
+        enterpriseName: {
+            type:String,
+            required:true 
+        },
+
+        socialSiege: {
+            type:String,
+            required:true,
+        },
+
+        capital: {
+            type: String,
+            required:true,
+        },
+
+        likers: {
+            type:[String],
+            required:true,
+        },
+
+        dislikers: {
+            type:[String],
+            required:true,
+        },
+
+        comments: {
+            type: [
+                {
+                    commenterId: String,
+                    commenterPseudo: String,
+                    text:String,
+                    timestamp: Number,
+                }
+            ],
+            required:true,
+        },
+    },
+
+    { timestamps: true,}
+)
+
+const enterpriseModel = mongoose.model("enterprise", enterpriseSchema)
+module.exports = enterpriseModel
---
+++ server/models/user.model.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose")
+const {isEmail} = require("validator")
+const bcrypt = require("bcrypt")
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type:String,
+            required:true,
+            minlength: 3,
+            maxlength: 55,
+            unique:true,
+            trim:true,
+        },
+
+        email: {
+            type:String,
+            required:true,
+            max:1024,
+            minlength:6,
+        },
+
+        picture: {
+            type:String,
+            default:"./uploads/profil/random-user.png"
+        },
+
+        bio:{
+            type:String,
+            max:500,
+        },
+
+        
+    },
+    {timestamps:true,}
+)
+
+userSchema.pre("save", async function(next) {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
+userSchema.static.login = async function(email,password){
+    const user = await this.findOne({email})
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password)
+        if (auth){
+            return user
+        }
+        throw Error("mot de passe incorrect")
+    }
+    throw Error("adresse mail incorrecte")
+}
+
+const userModel = mongoose.model("user", userSchema)
+module.exports = userModel
