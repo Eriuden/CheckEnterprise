f45740446b546bf29d759f3adcb0a155671ad2a8
Eriuden <antoinecarrette@hotmail.fr>
2023-01-26 23:47:30 +0100

    commentsDone

--- front/src/components/comment/EditDeleteComment.js
+++ front/src/components/comment/EditDeleteComment.js
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, {useState,useEffect, useContext} from 'react'
+import { useDispatch } from 'react-redux'
+import { deleteComment, editComment } from '../../redux/actions/enterprises.action'
+import { uidContext } from '../uidContext'
 
-export default function EditDeleteComment(comment) {
+
+export default function EditDeleteComment(comment, enterpriseId) {
+  const [isAuthor, setIsAuthor] = useState(false)
+  const [edit, setEdit] = useState(false)
+  const [text, setText] = useState(false)
+  const uid = useContext(uidContext)
+  const dispatch = useDispatch()
+
+  const handleEdit = (e) => {
+    e.preventDefault()
+
+    if (text) {
+      dispatch(editComment(postId, comment._id, text))
+      setText("")
+      setEdit(false)
+    }
+  }
+
+  const handleDelete = () => {
+    dispatch(deleteComment(enterpriseId, comment._id))
+  }
+
+  useEffect(()=> {
+    const checkAuthor = () => {
+      if (uid === comment.commenterId) {
+        setIsAuthor(true)
+      }
+    }
+    checkAuthor()
+  }, [uid, comment.commenterId])
+
   return (
-    <div>editDeleteComment</div>
+    <div>
+      {isAuthor && edit && (
+        <span onClick={ setEdit(!edit)}>
+          <img src='./img/icons/edit.svg' alt='edit' />
+        </span>
+      )}
+      {isAuthor && edit && (
+        <form action='' onSubmit={handleEdit} >
+          <label htmlFor='text' onClick={() => setEdit(!edit)}>
+            Editer
+          </label>
+          <br/>
+          <input type="text" name='text' onChange={(e)=> setText(e.target.value)}
+          defaultValue={comment.text}
+          />
+          <br/>
+          <div>
+            <span onClick={()=> {
+              if (window.confirm("voulez vous supprimer ce commentaire ?")) {
+                handleDelete()
+              }
+            }}>
+              <img src='img/icons/trash.svg' alt='' />
+            </span>
+          </div>
+          <input type="submit" value="valider les modifications" />
+        </form>
+      )}
+    </div>
   )
 }
--- front/src/components/comment/LikeDislikeButtons.js
+++ front/src/components/comment/LikeDislikeButtons.js
@@ -1,7 +1,73 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
+import { uidContext } from '../uidContext'
+import Popup from "reactjs-popup"
+import { likeEnterprise, unlikeEnterprise, dislikeEnterprise, undislikeEnterprise } from '../../redux/actions/enterprises.action'
 
-export default function likeDislikeButtons() {
+export default function likeDislikeButtons({enterprise}) {
+  const [liked, setLiked] = useState(false)
+  const [disliked, setDisliked] = useState(false)
+  const uid = useContext(uidContext)
+
+  const like = () => {
+    dispatchEvent(likeEnterprise(enterprise._id, uid))
+    setLiked(true)
+  }
+
+  const unlike = () => {
+    dispatchEvent(unlikeEnterprise(enterprise._id, uid))
+    setLiked(false)
+  }
+
+  const dislike = () => {
+    dispatchEvent(dislikeEnterprise(enterprise._id, uid))
+    setDisliked(true)
+  }
+
+  const undislike = () => {
+    dispatchEvent(undislikeEnterprise(enterprise._id, uid))
+    setDisliked(false)
+  }
+
+  useEffect(()=> {
+    if (enterprise.likers.includes(uid)) setLiked(true)
+    else if (enterprise.dislikers.includes(uid)) setDisliked(true)
+    else setLiked(false) , setDisliked(false)
+  }, [ uid, enterprise.likers, enterprise.dislikers, liked, disliked])
+
   return (
-    <div>likeDislikeButtons</div>
+    <div>
+
+      {
+        uid === null && (
+          <Popup trigger= {<img src="./img/icons/heart.svg" alt="like" />} position= {
+            ['bottom center', 'bottom right', 'bottom left']} closeOnDocumentClick >
+                <div>Vous devez être connecté pour liker un post</div>
+          </Popup> 
+         
+        )
+      }
+
+      {
+        uid && liked === false || disliked === false && (
+          <>
+            <img src="./img/icons/thumbUp.svg" onClick={like} alt="like"/>
+            <img src="./img/icons/thumbDown.svg" onClick={dislike} alt="dislike"/>
+          </>
+         
+        )
+      }
+
+      {
+        uid && liked || disliked && (
+          <>
+            <img src="./img/icons/thumbUp.svg" onClick={unlike} alt="like"/>
+            <img src="./img/icons/thumbDown.svg" onClick={undislike} alt="dislike"/>
+          </>   
+        )
+      }
+
+      <span>{enterprise.likers.length}</span>
+      <span>{enterprise.dislikers.length}</span>
+    </div>
   )
 }
